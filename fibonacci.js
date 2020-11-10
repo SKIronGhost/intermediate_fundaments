2afc9817cd375d108e5e7767df25df32e285ca48
SKIronGhost <ed.diegofelix@gmail.com>
2020-11-09 23:03:36 -0300

    update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,22 @@
 // Fibonacci - Crea una función para generar números de Fibonacci. En esta famosa secuencia matemática, cada número es la suma de las dos anteriores, partiendo con los valores 0 y 1. Tu función debería aceptar un argumento, un índice en la secuencia (donde 0 corresponde al valor inicial, 4 corresponden al valor cuatro más tarde, etc). Ejemplos: fibonacci(0) = 0 (dado), fibonacci(1) = 1 (dado), fibonacci(2) = 1 (fib(0)+fib(1), o 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, o 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8). Haz esto primero sin usar recursión. Si no sabes qué es una recursión, no te preocupes puesto que vamos a introducir este concepto en la Parte 2 de esta actividad. 
 
 function fibonacci(a){
-  let fibList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1.597] 
-  let fib = fibList[a]
-  return fib
+  if(a==0 || a==1){
+    return a
+  }
+  else{
+  let temp = 0//1,1
+  let sumFib = 1//1,2,3
+    for(i=2;i<=a;i++){
+      let a = sumFib//1,1,2
+      sumFib = sumFib + temp//1,2,3
+      temp = a//1,1,2
+    }
+    return sumFib
+  }
+//fibList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1.597] 
+
+  
 }
 
 console.log(fibonacci(0))
